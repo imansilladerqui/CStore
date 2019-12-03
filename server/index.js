0563651750ba6d76aae6c7e4d3cca23a54414104
imansilladerqui <imansilladerqui@hotmail.com>
2019-12-03 08:11:13 -0300

    refactor

--- server/index.js
+++ server/index.js
@@ -16,10 +16,7 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../public')))
-
-app.get('/', function (request, response){  
-  response.sendFile(path.resolve(__dirname, '../public', 'index.html')) })
+app.use(express.static('build'));
 
 app.use(session({
     secret: 'laksdnlasd',
