3d322563b2ae60157b6bcd57f0d5a43704dc45d9
imansilladerqui <imansilladerqui@hotmail.com>
2019-12-03 10:19:09 -0300

    lala

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 
+'index.html'));
+});
 app.use(session({
     secret: 'laksdnlasd',
     resave: false,
