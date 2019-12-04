a5da8a2acc4a62ef03c958299c24932b5e4fce57
imansilladerqui <imansilladerqui@hotmail.com>
2019-12-04 15:58:34 -0300

    debug

--- Components/user/network.js
+++ Components/user/network.js
@@ -5,12 +5,10 @@ const User = require('../auth/model');
 const _ = require('lodash');
 
 Router.get('/', (req, res) => {
-    if(!_.isEmpty(req.user)) {
-        return Promise.resolve(User.findOne({ where: {email: req.user.profile.emails[0].value}, raw: true})
-        .then(result => {
-            return res.send(result);
-        }));
-    }
+    return Promise.resolve(User.findOne({ where: {email: req.user.profile.emails[0].value}, raw: true})
+    .then(result => {
+        return res.send(result);
+    }));
 })
 
 module.exports = Router;
