258d99c59fcf9b5d0d13c863a9de1825745e019a
imansilladerqui <imansilladerqui@hotmail.com>
2019-12-06 18:30:02 -0300

    https

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,11 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-    app.use(proxy('/auth/google', { target: 'https://localhost:5000' }));
-    app.use(proxy('/auth/logout', { target: 'https://localhost:5000' }));
-    app.use(proxy('/btcvalue', { target: 'https://localhost:5000' }));
-    app.use(proxy('/clients/btcvalue', { target: 'https://localhost:5000' }));
-    app.use(proxy('/operation', { target: 'https://localhost:5000' }));
-    app.use(proxy('/user', { target: 'https://localhost:5000' }));
-    app.use(proxy('/profile', { target: 'https://localhost:5000' }));
+    app.use(proxy('/auth/google', {target: 'https://localhost:5000', changeOrigin: true, secure: false}));
+    app.use(proxy('/auth/logout', {target: 'https://localhost:5000', changeOrigin: true, secure: false}));
+    app.use(proxy('/btcvalue', {target: 'https://localhost:5000', changeOrigin: true, secure: false}));
+    app.use(proxy('/clients/btcvalue', {target: 'https://localhost:5000', changeOrigin: true, secure: false}));
+    app.use(proxy('/operation', {target: 'https://localhost:5000', changeOrigin: true, secure: false}));
+    app.use(proxy('/user', {target: 'https://localhost:5000', changeOrigin: true, secure: false}));
+    app.use(proxy('/profile', {target: 'https://localhost:5000', changeOrigin: true, secure: false}));
 };
