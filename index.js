a8beecc222f4688ba27129685afc77504a2a6208
imansilladerqui <imansilladerqui@hotmail.com>
2019-12-07 11:04:54 -0300

    bugs

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get('/pizarra', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 
