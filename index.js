acbc23bd775884d968c4f25d073faa5d9a2117df
imansilladerqui <imansilladerqui@hotmail.com>
2019-12-07 10:50:42 -0300

    bug

--- index.js
+++ index.js
@@ -18,14 +18,6 @@ app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-if(process.env.NODE_ENV === 'production') {
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 
-  'index.html'));
-  });
-}
-
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get('/pizarra', (req, res) => {
